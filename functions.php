d465e11ecb3d433b1c5ef2ba700f0f81fd45ed4a
Your Name <you@example.com>
2015-10-07 11:16:02 +0300

    esimene

--- functions.php
+++ functions.php
@@ -5,4 +5,27 @@
 	require_once("../config_global.php");
 	$database = "if15_mkoinc_3";
 	
+	function getCarData(){
+		
+		$mysqli = new mysqli($GLOBALS["servername"],$GLOBALS["server_username"],$GLOBALS["server_password"],$GLOBALS ["database"]);
+
+		$stmt = $mysqli -> prepare("SELECT id, user_id, number_id,number_plate,car_color");
+		
+				$stmt->bind_result($id, $user_id_from_database, $number_plate, $car_color);
+				$stmt ->execute();
+		
+		$row = 0;
+		
+		while($stmt->fetch()){
+			
+			echo $row." ".$number_plate."<br>";
+			$row++;
+			
+		}
+		
+		$stmt->close();
+		
+		$mysqli->close();
+	}
+	
 ?>
